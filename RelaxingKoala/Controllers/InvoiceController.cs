2dd7c2764a84d9726b9c954b728ad38f949cfae2
Jared Teng <jaredteng2002@gmail.com>
2024-05-31 13:38:35 +1000

    invoice controller

---
+++ RelaxingKoala/Controllers/InvoiceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using RelaxingKoala.Data;
+using RelaxingKoala.Models;
+using System.Security.Claims;
+
+namespace RelaxingKoala.Controllers
+{
+    [Authorize]
+    public class InvoiceController : Controller
+    {
+        private readonly InvoiceRepository invoiceRepo;
+        public InvoiceController(MySqlDataSource dataSource)
+        {
+            invoiceRepo = new InvoiceRepository(dataSource);
+        }
+        public IActionResult Index()
+        {
+            string? userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null) return RedirectToAction("Login", "Account");
+
+            List<Invoice> invoices = invoiceRepo.GetInvoicesByUserId(new Guid(userId));
+            return View(invoices);
+        }
+    }
+}
